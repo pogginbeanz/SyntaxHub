a5991edbbce85b3067f3cc5aeb39d53c1b18c891
Project-Soul <99675636+pogginbeanz@users.noreply.github.com>
2022-08-02 02:52:10 +0200

    Update Source.lua

--- UILibrary/Source.lua
+++ UILibrary/Source.lua
@@ -737,9 +737,9 @@ do
                 local function getPositionWithPadding(guiObject, direction, padding)
                     direction = direction:lower()
                     if direction == "top" then
-                        return UDim2.new(guiObject.Position.X.Scale, 0, guiObject.Position.Y.Scale, -(guiObject.Size.Y.Offset + padding))
+                        return UDim2.new(guiObject.Position.X.Scale, 0, guiObject.Position.Y.Scale, guiObject.Position.Y.Offset -(guiObject.Size.Y.Offset + padding))
                     elseif direction == "bottom" then
-                        return UDim2.new(guiObject.Position.X.Scale, 0, guiObject.Position.Y.Scale, (guiObject.Size.Y.Offset + padding))
+                        return UDim2.new(guiObject.Position.X.Scale, 0, guiObject.Position.Y.Scale, guiObject.Position.Y.Offset + (guiObject.Size.Y.Offset + padding))
                     end
                 end
 
