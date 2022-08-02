620de29a6963fe2e537eef8b62c8dc66994da7b5
Project-Soul <99675636+pogginbeanz@users.noreply.github.com>
2022-08-02 03:02:57 +0200

    Update Source.lua

--- UILibrary/Source.lua
+++ UILibrary/Source.lua
@@ -754,7 +754,7 @@ do
 
                 local function tweenLeft(tweenTime)
                     print(repr(Library.Notices, {pretty = true}))
-                    if not Library:GetActiveNotices()[NoticeIndex] then return end
+                    if not NoticeData.Active then return end
                     local newPosition = UDim2.new(-0.2, 0, notice.Position.Y.Scale, notice.Position.Y.Offset)
                     NoticeData.Active = false
                     notice:TweenPosition(newPosition, 'Out', 'Quad', tweenTime, true, function()
